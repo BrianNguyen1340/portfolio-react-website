ac89e62064cf1ca21dee830613037ac7eab5de29
BrianNguyen <anhkhongvui0@gmail.com>
2023-07-12 21:46:55 +0700

    portfolio react website

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import "./Footer.css"
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer__container container">
+        <h1 className="footer__title">
+          Sweet Candy
+        </h1>
+
+        <ul className="footer__list">
+          <li>
+            <a href="#about" className='footer__link'>
+              About
+            </a>
+          </li>
+
+          <li>
+            <a href="#porfolio" className='footer__link'>
+              Projects
+            </a>
+          </li>
+
+          <li>
+            <a href="#testimonial" className='footer__link'>
+              Testimonials
+            </a>
+          </li>
+        </ul>
+
+        <div className="footer__social">
+          <a href="https://www.facebook.com/" rel='noreferrer' className="home__socia-link" target="_blank">
+              <i className="uil uil-facebook-f color-black"></i>
+          </a>
+          <a href="https://twitter.com/" rel='noreferrer' className="home__social-link" target="_blank">
+              <i className="uil uil-twitter-alt color-black"></i>
+          </a>
+          <a href="https://github.com/" rel='noreferrer' className="home__social-link" target="_blank">
+              <i className="uil uil-github-alt color-black"></i>
+          </a>
+        </div>
+
+        <span className="footer__copy">&#169; Crypticalcoder. All rights reserved.</span>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
---
+++ src/components/home/Social.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Social = () => {
+  return (
+    <div className="home__social">
+        <a href="https://www.facebook.com/" rel='noreferrer' className="home__social-icon" target="_blank">
+            <i className="uil uil-facebook-f"></i>
+        </a>
+        <a href="https://twitter.com/" rel='noreferrer' className="home__social-icon" target="_blank">
+            <i className="uil uil-twitter-alt"></i>
+        </a>
+        <a href="https://github.com/" rel='noreferrer' className="home__social-icon" target="_blank">
+            <i className="uil uil-github-alt"></i>
+        </a>
+    </div>
+  )
+}
+
+export default Social
